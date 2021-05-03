00405fcea956eb31af1ef51fb0692cdb5e948104
nidhey27 <nidhey60@gmail.com>
2021-05-03 14:35:26 +0530

    make it better

--- routes/navbar.js
+++ routes/navbar.js
@@ -1,77 +1,117 @@
 const router = require('express').Router();
-
 const Navbar = require('../modals/Navbar');
 const SubMenu = require('../modals/SubMenu');
 const verifyToken = require('./verifyJWT')
+const ObjectID = require('mongodb').ObjectID;
 
-router.post('/add/menu', verifyToken ,async (req, res, next) => {
-    const navbar = new Navbar({
-        name: req.body.name,
-    })
+// Add Main Navbar and its sub-menu items
+/*
+    {
+        "name": "NAME", *required
+        "subMenu": [
+            { "name": "SUB-MENU Name" },
+            { "name": "SUB-MENU Name" },
+            { "name": "SUB-MENU Name" },
+        ]
+    }
 
+*/
+router.post('/add/menu', verifyToken, async (req, res, next) => {
     try {
-        let navbarData = await navbar.save();
+        const navbar = new Navbar({
+            name: req.body.name,
+        })
 
-        if(req.body.subMenu != undefined || req.body.subMenu != null){
-            
-            for(let i = 0; i < req.body.subMenu.length; i++){
-                // console.log(req.body.subMenu[i])
-                const subMenu = new SubMenu({
-                    name: req.body.subMenu[i].name,
-                    parentId: navbarData._id
-                })
+        try {
+            let navbarData = await navbar.save();
 
-                try {
-                    let subMenuData = await subMenu.save();
-                    // console.log("added");
-                } catch (err) {
-                    console.log(err)
-                    return res.status(200).json({ status: false, message: err.message })
+            if (req.body.subMenu != undefined || req.body.subMenu != null) {
+
+                for (let i = 0; i < req.body.subMenu.length; i++) {
+                    // console.log(req.body.subMenu[i])
+                    const subMenu = new SubMenu({
+                        name: req.body.subMenu[i].name,
+                        parentId: navbarData._id
+                    })
+
+                    try {
+                        let subMenuData = await subMenu.save();
+                        // console.log("added");
+                    } catch (err) {
+                        console.log(err)
+                        return res.status(200).json({ status: false, error: err.message })
+                    }
                 }
+
             }
 
+            return res.status(200).json({ status: true, message: "Added Successful", data: { navbarData } })
+        } catch (err) {
+            console.log(err)
+            return res.status(200).json({ status: false, error: err.message })
         }
-
-        return res.status(200).json({ status: true, message: "Added Successful", data: { navbarData } })
     } catch (err) {
         console.log(err)
-        return res.status(200).json({ status: false, message: err })
+        return res.status(200).json({ status: false, error: err.message })
     }
 })
 
-// router.post('/add/submenu', async (req, res, next) => {
-
-//     const subMenu = new SubMenu({
-//         name: req.body.name,
-//         parentId: req.body.parentId
-//     })
-
-//     try {
-//         let subMenuData = await subMenu.save();
-//         return res.status(200).json({ status: true, message: "Added Successful", data: { subMenuData } })
-//     } catch (err) {
-//         console.log(err)
-//         return res.status(200).json({ status: false, message: err.message })
-//     }
-// })
-
-router.get('/', verifyToken , async (req, res, next) => {
-    Navbar.aggregate([
-        {
-            $lookup: {
-                from: 'subMenu',
-                localField: '_id',
-                foreignField: 'parentId',
-                as: 'subNav'
+// Get MAIN NavBar
+/*
+    *required: headers: {
+        auth-token: "AUTH ACCESS TOKEN"
+    }
+*/
+router.get('/', async (req, res, next) => {
+    try {
+        Navbar.aggregate([
+            {
+                $lookup: {
+                    from: 'subMenu',
+                    localField: '_id',
+                    foreignField: 'parentId',
+                    as: 'subNav'
+                }
             }
-        }
-    ]).then(resp => {
-        res.send(resp);
-    }).catch(error => {
-        res.send(error)
-    })
+        ]).then(resp => {
+            res.send(resp);
+        }).catch(error => {
+            res.send(error)
+        })
+    } catch (err) {
+        console.log(err)
+        return res.status(200).json({ status: false, error: err.message })
+    }
 })
 
+// Update Menu
+/*
+    *required: ID in URL
+    {
+        "name" : "Name"
+    }
+*/
+router.put('/update/menu/:id', verifyToken, async (req, res, next) => {
+
+    try {
+        // Check if Entity Exists
+        const menuItemExists = await Navbar.findOne({ _id: ObjectID(req.params.id) })
+        if (!menuItemExists) return res.status(200).json({ status: false, message: `Item Not Found` })
+
+        let data = await Navbar.findByIdAndUpdate({ _id: ObjectID(req.params.id) }, {
+            $set: {
+                name: req.body.name
+            }
+        }, { upsert: true }).then(resp => {
+            return res.status(200).json({ status: true, message: "Updated!" })
+        }).catch(error => {
+            return res.status(200).json({ status: false, error: error.message })
+        })
+    } catch (error) {
+        return res.status(200).json({ status: false, error: error.message })
+    }
+
+})
 
 
 module.exports = router;
